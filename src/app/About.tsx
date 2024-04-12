035fa68bea591ff6264d10b96e82cfcd79dc4ec2
rohank36 <rohan.kanti@mail.mcgill.ca>
2024-04-11 22:59:43 -0400

    did our mission page

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import Link from 'next/link'
 
+//TODO: Change out all consulting biz to real biz name
 const About = () =>{
+    const rohanLinkedIn="https://www.linkedin.com/in/rohankanti/";
+    const ibroLinkedIn="https://www.linkedin.com/in/ibrahim-ayorinde-494b821ab/";
     return(
         <div >
             <div className="hero min-h-screen bg-white">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="flex flex-col ml-10">
                         <img src="/images/ntp.JPG" className="max-w-sm rounded-lg shadow-2xl" />
-                        <p className="text-center text-sm mt-2">Consulting Biz Co-Founders Ibrahim and Rohan winning the 2018 YMCA Peace Prize for the work they did with the Newcomers Tutoring Program</p>
+                        <p className="text-center text-sm mt-2">Consulting Biz Co-Founders Ibrahim (left) and Rohan (Middle) winning the 2018 YMCA Peace Prize</p>
                     </div>
                     <div>
-                    <h1 className="text-7xl font-bold">Our Mission</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi
-                    </p>
+                        <h1 className="text-7xl font-bold mb-3">Our Mission</h1>
+                        <div className='py-6'>
+                            <h1 className="text-3xl mb-5">At Consulting Biz, our mission is crystal clear: <div><b>We're here to pave the way for you to get recruited by your dream school.</b></div></h1>
+                            <h2 className="text-xl mb-5">You might wonder, <i>what fuels our drive?</i> The answer is simple - we have always been motivated by a heartfelt commitment to creating positive change in people's lives.</h2>
+                            <p>
+                                Back in 2017, driven by our deep passion for making a difference, we, <Link href={ibroLinkedIn} className="text-blue-500 underline">Ibrahim</Link> and <Link href={rohanLinkedIn} className="text-blue-500 underline">Rohan</Link>, launched the Newcomers Tutoring Program.   
+                                This initiative in its first year alone supported over 50 refugee children, easing their transition into the Canadian education system through personalized tutoring and peer support. 
+                                Our efforts were recognized in 2018 when we were honored with the YMCA Peace Medal, celebrating our commitment to community empowerment and our ability to make a significant impact with 
+                                minimal resources. Now, seven years later, our dedication to helping young adults unlock their potential remains unwavering. There's nothing we find more fulfilling than seeing you 
+                                pursue and achieve your dreams. 
+                            </p>
+                        </div>
                     </div>
                 </div>
                 </div>
