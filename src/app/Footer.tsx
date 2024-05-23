e0e2ef61d9b1a555c4ec032ad97f6944b1e23e4d
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2024-05-22 23:59:48 -0400

    Added new footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,11 +1,26 @@
+'use client';
+import { useRouter } from 'next/navigation';
 
 const Footer = () =>{
+    const router = useRouter()
     return(
         <div>
-            <footer className="footer footer-center p-10 bg-white text-slate-500">
-                <aside>
-                    <p>Copyright © 2024 - All rights reserved by ProspectPrep Consulting Ltd</p>
-                </aside>
+            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+                <nav className="grid grid-flow-col gap-4">
+                    <a className="link link-hover" type="button" onClick={() => router.push('/contact')}>Contact Us</a>
+                    <a className="link link-hover" type="button" onClick={() => router.push('/privacy')}>Privacy Policy</a>
+                </nav> 
+                <nav>
+                    <img src="/images/main2.png" className='h-16 rounded-lg'/>
+                </nav>
+                <div className="space-y-3 text-xs">
+                    <aside>
+                        <p>Suite 405-277 Lakeshore Road East</p>
+                    </aside>
+                    <aside>
+                        <p>Copyright © 2024 - All right reserved by ProspectPrep Consulting Ltd</p>
+                    </aside>
+                </div>
             </footer>
         </div>
     );
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ContactPage(){
+    return(
+        <div>
+            contact page 
+        </div>
+    );
+}
